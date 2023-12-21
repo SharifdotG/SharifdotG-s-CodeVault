d111df2c01eab8297d3a13f60e57980100dd32ff
Sharif Yousuf <sharifmdyousuf007@gmail.com>
2023-12-21 16:45:23 +0600

    Add missing line for notes array initialization

--- Beecrowd/Beginner/1000 - 1099/1018 - Banknotes/C++/1018_Banknotes.cpp
+++ Beecrowd/Beginner/1000 - 1099/1018 - Banknotes/C++/1018_Banknotes.cpp
@@ -9,6 +9,7 @@ int main() {
     cout << amount << endl;
 
     int notes[] = {100, 50, 20, 10, 5, 2, 1};
+    
     for (int i = 0; i < 7; i++) {
         int noteAmount = amount / notes[i];
         cout << noteAmount << " nota(s) de R$ " << notes[i] << ",00" << endl;
