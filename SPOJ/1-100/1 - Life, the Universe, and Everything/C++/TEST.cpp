9db1492d3a12b0587e532a51094e895a48bed189
Sharif Yousuf <sharifmdyousuf007@gmail.com>
2023-12-24 20:59:37 +0600

    Delete empty C++ file for SPOJ problem 1 - Life, the Universe, and Everything

---
+++ SPOJ/1-100/1 - Life, the Universe, and Everything/C++/TEST.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int number;
+    
+    while (cin >> number) {
+        if (number == 42) {
+            break;
+        }
+        
+        cout << number << endl;
+    }
+    
+    return 0;
+}
