6901fa24ceb23e50aee17a14b6aaf987ad1ae1a6
Sharif Yousuf <sharifmdyousuf007@gmail.com>
2024-01-01 18:58:27 +0600

    Remove empty C++ and Java files

---
+++ DMOJ/1 Points/Scroll/C++/scroll.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cout << "Can sample output exist without sample input? If a tree falls in a forest and no one is around to hear it, does it make a sound? Is the previous question even relevant?" << endl;
+    
+    return 0;
+}
