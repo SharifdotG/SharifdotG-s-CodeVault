93ad488a91af78ca3a1d4a83b2a356138b1051f0
Sharif Yousuf <sharifmdyousuf007@gmail.com>
2025-01-30 02:23:13 +0600

    Add initial solutions for Codeforces problems in C++

---
+++ Codeforces/Groups/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/A - Say Hello With C++/C++/A_SayHelloWithC++.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    cout << "Hello, " << s << endl;
+    
+    return 0;
+}
---
+++ Codeforces/Groups/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/B - Basic Data Types/C++/B_BasicDataTypes.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int int_value;
+    long long ll_value;
+    char char_value;
+    float float_value;
+    double double_value;
+    cin >> int_value >> ll_value >> char_value >> float_value >> double_value;
+
+    cout << int_value << endl;
+    cout << ll_value << endl;
+    cout << char_value << endl;
+    cout << float_value << endl;
+    cout << double_value << endl;
+    
+    return 0;
+}
